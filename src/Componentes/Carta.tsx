0836b4de48c30e23f5afbc60a788ba82d7aa4020
Joslis17 <joslism.17@gmail.com>
2025-11-09 20:53:25 -0400

    Actualización
    Se agregaron nuevos archivos y carpetas, sobre el proyecto de las cartas

---
+++ src/Componentes/Carta.tsx
@@ -0,0 +1,10 @@
+
+function Carta() {
+  return (
+    <div>
+        <h3>Carta 1</h3>
+    </div>
+  )
+}
+
+export default Carta
---
+++ src/Pantallas/VistaMazo.tsx
@@ -0,0 +1,12 @@
+import Carta from "../Componentes/Carta"
+
+function VistaMazo() {
+  return (
+    <div>
+        <h1>Mazo</h1>
+        < Carta />
+    </div>
+  )
+}
+
+export default VistaMazo
