3b1a265aa91d89cb3199416d4c2e9999146f2f34
Joslis17 <joslism.17@gmail.com>
2025-11-22 05:49:03 -0400

    Estructura de la vistaCarta
    Se agregaron imagenes de las cartas y se hicieron los detalles que iba en las cartas

--- src/Componentes/Carta.tsx
+++ src/Componentes/Carta.tsx
@@ -1,10 +1,42 @@
+import './Carta.css'
 
-function Carta() {
+type props = {
+  numero: number;
+  nombre: string;
+  tipo: string;
+  ataque?: number;
+  defensa: number;
+  descripcion: string;
+  imagen: string;
+  vida: number;
+
+}
+
+function Carta({
+  ataque=0,
+  defensa,
+  descripcion,
+  imagen,
+  nombre,
+  numero,
+  tipo,
+  vida,
+}: props) {
   return (
     <div>
-        <h3>Carta 1</h3>
+
+      <h3>
+        {nombre} (#{numero})
+      </h3>
+      <img src={imagen} alt={nombre} />
+      <p>Tipo: {tipo}</p>
+      <p>Ataque: {ataque}</p>
+      <p>Defensa: {defensa}</p>
+      <p>Vida: {vida}</p>
+      <p>{descripcion}</p>
+
     </div>
-  )
+  );
 }
 
 export default Carta
--- src/Pantallas/VistaMazo.tsx
+++ src/Pantallas/VistaMazo.tsx
@@ -1,10 +1,34 @@
+import './VistaMazo.css'
+
 import Carta from "../Componentes/Carta"
+import Payaso_It from '../assets/Imagenes/Cartas/Payaso_It.jpg'
+import La_Monja from '../assets/Imagenes/Cartas/La_Monja.jpg'
 
 function VistaMazo() {
   return (
     <div>
         <h1>Mazo</h1>
-        < Carta />
+        <Carta
+          numero={1}
+          nombre="Payaso It"
+          imagen= {Payaso_It}
+          tipo="Payaso malvado"
+          ataque={3000}
+          defensa={2500}
+          vida={80}
+          descripcion="Un payaso terrorífico que se alimenta del miedo, y crea ilusiones muy reales para atrapar a sus víctimas."
+          
+        />
+        <Carta
+          numero={2}
+          nombre="La Monja"
+          imagen= {La_Monja}
+          tipo="Demonio"
+          ataque={3500}
+          defensa={2000}
+          vida={100}
+          descripcion="Un demonio que usa la apariencia de una monja para engañar a sus víctimas y usa su fuerza física sobrenatural y la capacidad de poseer personas. "
+        />
     </div>
   )
 }
